1d98e6c1c442537d1cbeaabad2ee3f6aded797f7
Gary Hoang <garyhoangvinh@gmail.com>
2022-07-21 14:48:06 -0700

    2

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 let hex = true;
 let rgb = [0, 0, 0];
 let hexCode = "";
+let random_colors = [];
 let feedback = document.querySelector(".container p");
 let main = document.querySelector("main");
+let select = document.querySelector("select");
+
+const NUM_LIST_COLORS = 5;
+
+let showColor = function (event) {
+    let color = event.target.value;
+    feedback.textContent = color;
+    main.style.backgroundColor = color;
+}
+
+let generateRandomColors = function () {
+    for (let i = 0; i < NUM_LIST_COLORS; i++) {
+        rgb[0] = Math.floor(Math.random() * (255 + 1));
+        rgb[1] = Math.floor(Math.random() * (255 + 1));
+        rgb[2] = Math.floor(Math.random() * (255 + 1));
+        let newColor = (rgb[0].toString(16).padStart(2, '0') + rgb[1].toString(16).padStart(2, '0') + rgb[2].toString(16).padStart(2, '0')).toUpperCase();
+        random_colors[i] = "#" + newColor;
+        let inverse_color = ((255 - rgb[0]).toString(16).padStart(2, '0') + (255 - rgb[1]).toString(16).padStart(2, '0') + (255 - rgb[2]).toString(16).padStart(2, '0')).toUpperCase();
+
+        let option = document.createElement("option");
+        option.value = "#" + newColor;
+        option.style.backgroundColor = "#" + newColor;
+        option.innerText = "#" + newColor;
+        option.style.color = inverse_color;
+        // option.onselect = (e) => showColor(e);
+        select.onchange = showColor;
+        select.appendChild(option);
+    }
+    // console.log(random_colors);
+}
+
+generateRandomColors();
 
 let randomizeColor = function () {
     rgb[0] = Math.floor(Math.random() * (255 - 0 + 1));
